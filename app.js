28d7f162582077525ffcf56dca6a4e857aadec04
Mauricio Arias <mauricio.arias@geniussports.com>
2022-01-17 20:15:47 -0500

    Chap 2 - Working with static data

--- app.js
+++ app.js
@@ -2,22 +2,46 @@ const express = require('express')
 const app = express()
 const { ApolloServer, gql } = require('apollo-server-express')
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core')
+const users = require('./data').users
+const cars = require('./data').cars
+const me = users[0]
 
 const typeDefs = gql`
   type Query {
+    users: [User]
+    user(id: Int!): User
     me: User
+
+    cars: [Car]
+    car(id: Int!): Car
   }
 
   type User {
+    id: ID!
     name: String!
+  }
+
+  type Car {
+    id: ID!,
+    make: String!
+    model: String!
+    colour: String!
   }
 `
 const resolvers = {
   Query: {
-    me: () => {
-      return {
-        name: 'Mauro'
-      }
+    users: () => users,
+    user: (parent, { id }, context, info) => {
+      console.log(parent)
+      console.log(id)
+      const user = users.filter(user => user.id === id)
+      console.log(user)
+      return user[0]
+    },
+    me: () => me,
+    cars: () => cars,
+    car: (parent, { id }) => {
+      return cars.filter(car => car.id === id)[0]
     }
   }
 }
---
+++ data.js
@@ -0,0 +1,44 @@
+let users = [
+  {
+    id: 1,
+    name: 'Mauro'
+  },
+  {
+    id: 2,
+    name: 'Sofi'
+  },
+  {
+    id: 3,
+    name: 'Gladys'
+  },
+  {
+    id: 4,
+    name: 'Luisa'
+  }
+]
+
+let cars = [
+  {
+    id: 1,
+    make: 'Mazda',
+    model: '3',
+    colour: 'titanium'
+  },
+  {
+    id: 2,
+    make: 'Ford',
+    model: 'Focus',
+    colour: 'red'
+  },
+  {
+    id: 3,
+    make: 'Chevrolet',
+    model: 'Camaro',
+    colour: 'yellow'
+  }
+]
+
+module.exports = {
+  users,
+  cars
+}
